9823be5b1b3a539bbf5a7bcddf21e49acde01f1d
ronanduddy <ronanduddy@live.ie>
2021-01-18 16:37:56 +0000

    Invoke set up first

--- test/test.rb
+++ test/test.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class TestCase
   AssertionError = Class.new(RuntimeError)
 
@@ -12,30 +14,50 @@ class TestCase
   end
 
   def run
+    set_up
     send @name
+  end
+
+  def set_up
+    @was_run = nil
+    @was_set_up = true
   end
 end
 
 class WasRun < TestCase
-  attr_reader :was_run
+  attr_reader :was_run, :was_set_up
 
   def initialize(name)
     @was_run = false
+    @was_set_up = false
+
     super(name)
   end
 
   def test_method
     @was_run = true
   end
+
+  def was_set_up
+    @was_set_up = true
+  end
 end
 
 class TestCaseTest < TestCase
+  def set_up
+    @test = WasRun.new('test_method')
+  end
+
   def test_running
-    test = WasRun.new('test_method')
-    assert { !test.was_run }
-    test.run
-    assert { test.was_run }
+    @test.run
+    assert { @test.was_run }
+  end
+
+  def test_set_up
+    @test.run
+    assert { @test.was_set_up }
   end
 end
 
 TestCaseTest.new('test_running').run
+TestCaseTest.new('test_set_up').run
